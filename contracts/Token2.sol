0de50c7e82600ef437573f403c60229f8f94c89c
dhylan01 <dhylan01@users.noreply.github.com>
2021-12-17 07:42:45 -0800

    bug fixes

--- contracts/Token2.sol
+++ contracts/Token2.sol
@@ -19,18 +19,33 @@ import "hardhat/console.sol";
 
 //Trying to import ERC 2771 Functions for use
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+//node_modules\@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol
 
 //bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 //bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-contract CustomTokyoFizz is ERC721 {
+contract CustomTokyoFizz is
+    ERC721,
+    AccessControl,
+    ERC721Enumerable,
+    ERC721Pausable
+{
     uint256 constant limit = 3000;
     //uint256 const waitlistLimit = 200;
     //mapping for whilelist - should be checked by reading blockchain so no gas used ehre
     mapping(address => bool) public whitelisted;
 
     //every time someone mints we incriment this to reflect the current token we are on for minting
-    Counters.Counter internal _tokenIdTracker;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdTracker;
+    string private _baseTokenURI;
 
     //creating a new ERC 721 object here
     constructor(
@@ -41,7 +56,7 @@ contract CustomTokyoFizz is ERC721 {
     ) ERC721(name, symbol) {
         //ipfs hash for the collection meta data itself
         _baseTokenURI = baseTokenURI;
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole("DEFAULT_ADMIN_ROLE", _msgSender());
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
@@ -52,16 +67,13 @@ contract CustomTokyoFizz is ERC721 {
         return baseURI;
     }
 
-    function setBaseURI(string memory baseTokenURI)
-        public
-        requires
-        DEFAULT_ADMIN_ROLE
-    {
+    function setBaseURI(string memory baseTokenURI) public virtual {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
         _baseTokenURI = baseTokenURI;
     }
 
     // look at timestamps and make them accurate based on the right time for minting
-    function mint(uint256 amount) internal payable {
+    function mint(uint256 amount) public payable {
         require(amount > 50000000, "amount is not .05 ETH");
         require(
             _tokenIdTracker.current() < limit && msg.sender.balanceOf() < 1,
@@ -76,7 +88,8 @@ contract CustomTokyoFizz is ERC721 {
         msg.sender.transfer(amount);
     }
 
-    function mint(address to) public requires DEFAULT_ADMIN_ROLE {
+    function mint(address to) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
         require(
             _tokenIdTracker.current() < limit,
             "CustomTokyoFizz: limit exceeded"
@@ -106,24 +119,23 @@ contract CustomTokyoFizz is ERC721 {
         whitelisted[a] = true;
     }
 
-    function newAdmin(address a) public requires DEFAULT_ADMIN_ROLE {
-        _setupRole(DEFAULT_ADMIN_ROLE, a);
+    function newAdmin(address a) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
+        _grantRole(DEFAULT_ADMIN_ROLE, a);
     }
 
-    function pause() public requires DEFAULT_ADMIN_ROLE {
+    function pause() public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
         _pause();
     }
 
-    function unpause() public requires DEFAULT_ADMIN_ROLE {
+    function unpause() public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
         _unpause();
     }
 
-    function transferBalence(uint256 amount)
-        public
-        payable
-        requires
-        DEFAULT_ADMIN_ROLE
-    {
+    function transferBalence(uint256 amount) public payable {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
         address(this).transfer(amount * address(this).balance);
     }
 }
